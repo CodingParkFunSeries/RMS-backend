d2f12488e1f572ef085ae36afed02201c69fc8e2
DEEPA <cherideepa@gmail.com>
2019-11-04 22:44:36 +0530

    [Deepa] Add. Sample Endpoints

---
+++ src/test/java/com/codingParkFun/rmsbackend/RmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingParkFun.rmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
